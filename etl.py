ae481804f14c7160fb1cc0197fc1e4966a77b334
self <cloudshell-user@ip-10-0-41-73.ec2.internal>
2021-02-05 15:55:27 +0000

    adding data

--- etl.py
+++ etl.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import io
+import zipfile
 from io import BytesIO
+import requests
 
 r = requests.get("https://databank.worldbank.org/data/download/BBSC_CSV.zip", stream=True)
 z = zipfile.ZipFile(io.BytesIO(r.content))
 z.extractall()
 
 df = pd.read_csv("BBSCData.csv")
-df.head()
+print(df.head())
