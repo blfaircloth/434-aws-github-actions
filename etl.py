134089d3c96e7c3e43fabc654efb46120b12ff3b
Ben <59846896+blfaircloth@users.noreply.github.com>
2021-02-05 10:00:15 -0500

    Create etl.py

---
+++ etl.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import requests, zipfile, io
+from io import BytesIO
+
+r = requests.get("https://databank.worldbank.org/data/download/BBSC_CSV.zip", stream=True)
+z = zipfile.ZipFile(io.BytesIO(r.content))
+z.extractall()
+
+df = pd.read_csv("BBSCData.csv")
+df.head()
