2f2587103a7a39c0ce597f331bdc20190298c6e0
Ben <59846896+blfaircloth@users.noreply.github.com>
2021-02-05 15:57:12 -0500

    Update etl.py

--- etl.py
+++ etl.py
@@ -3,10 +3,24 @@ import io
 import zipfile
 from io import BytesIO
 import requests
+import boto3
 
+# Request data from world bank - unzip
 r = requests.get("https://databank.worldbank.org/data/download/BBSC_CSV.zip", stream=True)
 z = zipfile.ZipFile(io.BytesIO(r.content))
 z.extractall()
 
+
+# Load data to pandas df
 df = pd.read_csv("BBSCData.csv")
 print(df.head())
+
+
+#s3 = boto3.resource('s3')
+#bucket = s3.Bucket('434-etl-target')
+#s3.object('434-etl-target', 'BBSCdata.csv').put(Body=open('tmp/BBSCData.csv')) 
+
+
+s3 = boto3.client('s3')
+with open("BBSCData.csv", "rb") as f:
+        s3.upload_fileobj(f, "434-etl-target", "BBSCData.csv")
