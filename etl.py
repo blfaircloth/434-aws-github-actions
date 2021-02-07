390015bb71915f50c778554ec54a35ce89d349ed
Ben <59846896+blfaircloth@users.noreply.github.com>
2021-02-07 10:16:33 -0500

    Clean headers

--- etl.py
+++ etl.py
@@ -4,6 +4,10 @@ import zipfile
 from io import BytesIO
 import requests
 import boto3
+# import boto.s3
+
+# from boto3.s3.key import Key
+# from secret.py import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
 # Request data from world bank - unzip
 r = requests.get("https://databank.worldbank.org/data/download/BBSC_CSV.zip", stream=True)
@@ -13,14 +17,11 @@ z.extractall()
 
 # Load data to pandas df
 df = pd.read_csv("BBSCData.csv")
-print(df.head())
-
-
-#s3 = boto3.resource('s3')
-#bucket = s3.Bucket('434-etl-target')
-#s3.object('434-etl-target', 'BBSCdata.csv').put(Body=open('tmp/BBSCData.csv')) 
+df.columns = df.columns.str.replace(' ', '_')
+df = df.rename(columns={'2018':'Y2018'})
+df.to_csv('BBSCData-clean.csv', index=False)
 
 
 s3 = boto3.client('s3')
-with open("BBSCData.csv", "rb") as f:
-        s3.upload_fileobj(f, "434-etl-target", "BBSCData.csv")
+with open("BBSCData-clean.csv", "rb") as f:
+        s3.upload_fileobj(f, "434-etl-target", "BBSCData-clean.csv")
